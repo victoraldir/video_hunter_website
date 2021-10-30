5b702cada3ecb5fea415f98d76e915ba17e2821f
victoraldir <vhaldir@gmail.com>
2021-10-29 23:25:56 -0300

    Inicial commit

---
+++ js/main.js
@@ -0,0 +1,123 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+
+function addVideo(video) {
+
+    const content = document.createTextNode('Your browser does not support the video tag.');
+    const videoUrl = filterByContentType(video.variants, 'video/mp4').url
+
+    let colunmEl = document.createElement('div');
+    colunmEl.classList.add('col')
+    colunmEl.classList.add('text-center')
+
+    let cardElement = document.createElement('div');
+    cardElement.classList.add('tweet-card')
+
+    let videoDivElement = document.createElement('div');
+    videoDivElement.id = video.tweet_id
+
+    let videoThumbEl = document.createElement('img');
+    videoThumbEl.setAttribute('onclick', "replaceImgToVideo(" + video.tweet_id + ", '" + videoUrl + "')")
+    videoThumbEl.src = video.video_img
+    videoThumbEl.classList.add('tweet-card-image')
+
+    let cardBodyEl = document.createElement('div');
+    cardBodyEl.classList.add('card-body')
+
+    let downloadBtnEl = document.createElement('a');
+    downloadBtnEl.setAttribute('onclick', "downloadVideo('" + videoUrl + "')")
+    downloadBtnEl.classList.add('btn')
+    downloadBtnEl.classList.add('btn-primary')
+    downloadBtnEl.appendChild(document.createTextNode('Download'))
+
+    let pEl1 = document.createElement('p');
+    pEl1.appendChild(downloadBtnEl)
+
+    let originalBtnEl = document.createElement('a');
+    originalBtnEl.setAttribute('href', video.video_url)
+    originalBtnEl.setAttribute('target', '_blank')
+    originalBtnEl.classList.add('btn')
+    originalBtnEl.classList.add('btn-primary')
+    originalBtnEl.appendChild(document.createTextNode('Original Tweet'))
+
+    let pEl2 = document.createElement('p');
+    pEl2.appendChild(originalBtnEl)
+
+    videoDivElement.appendChild(videoThumbEl)
+    cardBodyEl.appendChild(pEl1)
+    cardBodyEl.appendChild(pEl2)
+    cardElement.appendChild(videoDivElement)
+    cardElement.appendChild(cardBodyEl)
+    colunmEl.appendChild(cardElement)
+
+    document.getElementById('video-grid').appendChild(colunmEl);
+}
+
+function downloadVideo(videoUrl) {
+    let oReq = new XMLHttpRequest();
+    oReq.responseType = 'blob';
+    oReq.onload = function(e) {
+        var blob = e.currentTarget.response;
+        var fileName = 'video.mp4'
+        saveBlob(blob, fileName);
+    };
+    oReq.open("get", videoUrl, true);
+    oReq.send();
+}
+
+function saveBlob(blob, fileName) {
+    let a = document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = fileName;
+    a.dispatchEvent(new MouseEvent('click'));
+}
+
+function loadVideos() {
+    let videoList = JSON.parse(this.responseText);
+    videoList.Items.forEach(function(video) {
+        addVideo(video);
+    })
+}
+
+function filterByContentType(variants, contentType) {
+    let mp4List = variants.filter(function(variant) {
+        return variant.content_type === contentType;
+    })
+
+    let greatest = 0
+
+    mp4List.forEach(function(variant) {
+        if (variant.bitrate > greatest) {
+            greatest = variant
+        }
+    })
+
+    return greatest
+}
+
+function replaceImgToVideo(id, videoUrl) {
+
+    var element = document.getElementById(id);
+    var videlem = document.createElement("video");
+    element.innerHTML = '';
+    source = videoUrl;
+    videlem.classList.add('video-player')
+
+    videlem.setAttribute('controls', '')
+    var sourceMP4 = document.createElement("source");
+    sourceMP4.type = "video/mp4";
+    sourceMP4.src = source;
+    videlem.appendChild(sourceMP4);
+    videlem.autoplay = true;
+    element.appendChild(videlem);
+}
+
+function fetchVideos() {
+    let oReq = new XMLHttpRequest();
+    oReq.onload = loadVideos;
+    oReq.open("get", "https://api.myvideohunter.com/requester/" + params.u + "/list", true);
+    oReq.send();
+}
+
+window.onload = fetchVideos;
